2e24ad22c16375bb2d47698bf0ebf67e843a2c92
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2019-09-20 20:18:02 +0000

    Add COPY commands.

--- evaluation/scripts/sql/select_execution_mode_changes.sql
+++ evaluation/scripts/sql/select_execution_mode_changes.sql
@@ -1,29 +1,31 @@
-SELECT method_change_detail.project, 
-       method_change_detail.sha, 
-       method_change_detail.language, 
-       method_change_detail.file_name, 
-       method_change_detail.is_test, 
-       method_change_detail.method_name, 
-       method_change_detail.parallel___adds, 
-       method_change_detail.parallel___dels, 
-       method_change_detail."parallelStream___adds", 
-       method_change_detail."parallelStream___dels", 
-       method_change_detail.sequential___adds, 
-       method_change_detail.sequential___dels, 
-       method_change_detail.total_adds, 
-       method_change_detail.total_dels, 
-       method_change_detail.warning_alert, 
-       change_summary.is_bug 
-FROM   method_change_detail 
-       INNER JOIN change_summary 
-               ON method_change_detail.sha = change_summary.sha 
-WHERE  method_change_detail.parallel___adds > 0
-        OR method_change_detail.parallel___dels > 0
-        OR method_change_detail.sequential___adds > 0
-        OR method_change_detail.sequential___dels > 0
-        OR method_change_detail."parallelStream___adds" > 0
-        OR method_change_detail."parallelStream___dels" > 0
-ORDER  BY method_change_detail.project DESC,
-          method_change_detail.sha,
-          file_name,
-          method_name
+COPY (
+	SELECT method_change_detail.project, 
+	       method_change_detail.sha, 
+	       method_change_detail.language, 
+	       method_change_detail.file_name, 
+	       method_change_detail.is_test, 
+	       method_change_detail.method_name, 
+	       method_change_detail.parallel___adds, 
+	       method_change_detail.parallel___dels, 
+	       method_change_detail."parallelStream___adds", 
+	       method_change_detail."parallelStream___dels", 
+	       method_change_detail.sequential___adds, 
+	       method_change_detail.sequential___dels, 
+	       method_change_detail.total_adds, 
+	       method_change_detail.total_dels, 
+	       method_change_detail.warning_alert, 
+	       change_summary.is_bug 
+	FROM   method_change_detail 
+	       INNER JOIN change_summary 
+		       ON method_change_detail.sha = change_summary.sha 
+	WHERE  method_change_detail.parallel___adds > 0
+		OR method_change_detail.parallel___dels > 0
+		OR method_change_detail.sequential___adds > 0
+		OR method_change_detail.sequential___dels > 0
+		OR method_change_detail."parallelStream___adds" > 0
+		OR method_change_detail."parallelStream___dels" > 0
+	ORDER  BY method_change_detail.project DESC,
+		  method_change_detail.sha,
+		  file_name,
+		  method_name
+) TO STDOUT with CSV HEADER
